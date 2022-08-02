79e483a8449d854b4f32b3d63d6bec48d14a4a0d
Joao Andrade Jr <fullservicemanaus@gmail.com>
2022-08-02 16:09:53 -0400

    feat: get character info via api

--- src/Redux/reducers/charactersReducer.js
+++ src/Redux/reducers/charactersReducer.js
@@ -1,9 +1,16 @@
+import { SET_CHARACTER_DETAILS } from "../actions/actionTypes";
+
 const INITIAL_STATE = {
   characterDetails: undefined,
-};
+}
 
 function charactersReducer(state = INITIAL_STATE, action) {
   switch(action.type) {
+  case SET_CHARACTER_DETAILS:
+    return {
+      ...state,
+      characterDetails: action.payload,
+    };
   default:
     return state;
   }
